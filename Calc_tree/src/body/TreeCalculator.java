2712e6eec6280bf72554163b2cb4eb8f5dd9a035
Victor <Victor@Misto>
2016-06-08 16:16:56 -0300

    BIR leitura arquivo

--- Calc_tree/src/body/TreeCalculator.java
+++ Calc_tree/src/body/TreeCalculator.java
@@ -1,8 +1,31 @@
 package body;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TreeCalculator {
-	public static void main(String args[]){
-		Tree<Integer> t = new Tree<>();
-		t.loadFile();
+	public static void main(String args[]) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader("expressoes.txt"));
+		String line;		
+		
+	/*	while((line = br.readLine()) != null) {
+			int contaParenteses = 0;
+			String parts[]= line.split(" ");
+			
+			for(int i = 0; i<parts.length; i++){
+				if(parts[i].equals("("))
+					contaParenteses++;
+				if(parts[i].equals(")"))
+					contaParenteses--;  
+			} 
+			if(contaParenteses != 0) {
+					System.out.println("Erro de Sintaxe!");
+					System.out.println(" ");
+					continue;
+				}
+		} */
+		
 	}
 }
